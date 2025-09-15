38f5e74b2416789eca85d94dd7d0aef87d8abbba
tchen <tepelchen501@gmail.com>
2025-09-16 06:29:18 +0900

    hi

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -19,7 +19,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: "iam",
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
